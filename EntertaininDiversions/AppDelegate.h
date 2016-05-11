a33e5998c330f6124248b374fcfb3fac3858ff34
DeanMr <deanlzwei@gmail.com>
2016-05-11 10:32:59 +0800

    Initial Commit

---
+++ EntertaininDiversions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EntertaininDiversions
+//
+//  Created by 云宝 Dean on 16/5/11.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EntertaininDiversions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EntertaininDiversions
+//
+//  Created by 云宝 Dean on 16/5/11.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
